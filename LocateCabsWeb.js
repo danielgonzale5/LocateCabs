bb97ef97d4de8f2e81688e5eeac4cdab2f9ff146
jricoj <jricoj@uninorte.edu.co>
2021-09-29 19:54:26 -0500

    Test_realtime2

--- LocateCabsWeb.js
+++ LocateCabsWeb.js
@@ -1,15 +1,22 @@
-var app    = require('express')();
+const express = require('express');
+var app = require('express')();
 var server = require('http').createServer(app);
+var systemchild = require("child_process");
 
 const port = 3000
 var DatosGPS;
-var Datatotalgps
+
 var udp = require('dgram');
 
 var dir = __dirname;
 
-app.get('/', function(req, res) {
-    res.sendfile(dir + '/index.html');
+app.post('/github', function (req, res) {
+  console.log("received")
+  systemchild.exec("cd /home/ubuntu/LocateCabs && git reset --hard && git pull")
+});
+
+app.get('/', function (req, res) {
+  res.sendfile(dir + '/index.html');
 });
 
 app.get('/routing', function(req, res) {
@@ -18,12 +25,12 @@ app.get('/routing', function(req, res) {
 
 var io = require('socket.io')(server);
 
-server.listen(port, function(error) {
-    if (error) {
-        console.log('Hay un error', error)
-    } else {
-        console.log('El servidor esta escuchando el puerto ' + port)
-    }
+server.listen(port, function (error) {
+  if (error) {
+    console.log('Hay un error', error)
+  } else {
+    console.log('El servidor esta escuchando el puerto ' + port)
+  }
 })
 
 //Variables de entorno
@@ -42,126 +49,128 @@ const mysql = require('mysql')
 var data;
 
 var con = mysql.createConnection({
-    host: entvar.parsed.DB_HOST,
-    user: entvar.parsed.DB_USER,
-    password: entvar.parsed.DB_PASS,
-    database: 'locatecabs'
-})   
-
-con.connect((err) =>{
-    if(err) {
-        console.log('hay un error de conexión con la base de datos')
-    }else{
-        console.log('la conexión con la base de datos funciona')
-    } 
+  host: entvar.parsed.DB_HOST,
+  user: entvar.parsed.DB_USER,
+  password: entvar.parsed.DB_PASS,
+  database: 'locatecabs'
 })
 
-var Imysql = "INSERT INTO gps (Usuario, Latitud, Longitud, Fecha, Hora) VALUES ?";
+con.connect((err) => {
+  if (err) {
+    console.log('hay un error de conexión con la base de datos')
+  } else {
+    console.log('la conexión con la base de datos funciona')
+  }
+})
+
+var Imysql = "INSERT INTO gps (Usuario, Latitud, Longitud, TimeStamp) VALUES ?";
 var values = [
-    ["-","-","-","-","-"],
-  ];
+  ["-", "-", "-", "-"],
+];
+
 con.query(Imysql, [values], function (err) {
-if (err) throw err;
+  if (err) throw err;
 });
+
+
 
 // creating a udp server
 var serverudp = udp.createSocket('udp4');
 
+
+
 // emits when any error occurs
-serverudp.on('error',function(error){
+serverudp.on('error', function (error) {
   console.log('Error: ' + error);
   serverudp.close();
 });
 
 // emits on new datagram msg
-serverudp.on('message',function(msg,info){ 
+serverudp.on('message', function (msg, info) {
   console.log('Data received from client : ' + msg.toString());
-  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
-//sending msg
-serverudp.send(msg,info.port,'localhost',function(error){
-  if(error){
-    client.close();
-  }else{
-    console.log('Data sent !!!');
-  }
-});
+  console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
+  //sending msg
+  serverudp.send(msg, info.port, 'localhost', function (error) {
+    if (error) {
+      client.close();
+    } else {
+      console.log('Data sent !!!');
+    }
+  });
 
-DatosGPS = msg.toString().split(";")
- 
+  DatosGPS = msg.toString().split(";")
 
+  var Imysql = "INSERT INTO gps (Usuario, Latitud, Longitud, TimeStamp) VALUES ?";
+  var values = [
+    [DatosGPS[0], DatosGPS[1], DatosGPS[2], DatosGPS[3]]];
+  con.query(Imysql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Records inserted: " + result.affectedRows);
 
-// 
-    Datatotalgps = parseFloat(DatosGPS[3])
-    let unix_timestamp = Datatotalgps
-    var date = new Date(unix_timestamp);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var month = months[date.getMonth()];
-    var hours = date.toLocaleTimeString('en-GB',{timeZone:'America/Bogota'});
-    var minutes = "0" + date.getMinutes();
-    var seconds = "0" + date.getSeconds();
-    var formattedTime = hours.substr(0,2) + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    var tot1 = formattedTime.toString() ; 
-    var tot2 =  date.getDate().toString()+"/"+  month.toString()+"/"+date.getFullYear().toString() ; 
-
-//
-
-
-
-
-var Imysql = "INSERT INTO gps (Usuario, Latitud, Longitud, Fecha, Hora) VALUES ?";
-var values = [
-    [DatosGPS[0],DatosGPS[1], DatosGPS[2],tot2,tot1],];
-con.query(Imysql, [values], function (err, result) {
-if (err) throw err;
-console.log("Records inserted: " + result.affectedRows);
-
-});
+  });
 });
 //emits after the socket is closed using socket.close();
-serverudp.on('close',function(){
+serverudp.on('close', function () {
   console.log('Socket is closed !');
 });
 
 serverudp.bind(3020);
 
-setTimeout(function(){
-serverudp.close();
-},999999999);
+setTimeout(function () {
+  serverudp.close();
+}, 999999999);
 
-setInterval(function(){
-con.query('SELECT * FROM gps ORDER BY idGPS DESC LIMIT 1', function(err, rows) {
-  if(err) throw err;
-  data = JSON.parse(JSON.stringify(rows))
-  var dataGPS = Object.values(data[0])
-  var DataUsu = dataGPS[1]
-  var DataLat = parseFloat(dataGPS[2]).toFixed(6)
-  var DataLong = parseFloat(dataGPS[3]).toFixed(6)
-  var DataFecha = dataGPS[4]
-  var DataHora = dataGPS[5]
+setInterval(function () {
+  con.query('SELECT * FROM gps ORDER BY idGPS DESC LIMIT 1', function (err, rows) {
+    if (err) throw err;
+    data = JSON.parse(JSON.stringify(rows))
+    var dataGPS = Object.values(data[0])
+    var DataUsu = dataGPS[1]
+    var DataLat = parseFloat(dataGPS[2]).toFixed(6)
+    var DataLong = parseFloat(dataGPS[3]).toFixed(6)
+    var DataTime = parseFloat(dataGPS[4])
 
-  io.emit('change', {
-      DataUsu :DataUsu, 
+
+    io.emit('change', {
+      DataUsu: DataUsu,
       DataLat: DataLat,
       DataLong: DataLong,
-      DataFecha: DataFecha,
-      DataHora: DataHora
+      DataTime: DataTime,
 
+    });
+
+    io.on('connection', function (socket) {
+      socket.emit('change', {
+        DataUsu: DataUsu,
+        DataLat: DataLat,
+        DataLong: DataLong,
+        DataTime: DataTime,
+      });
       
+    });
   });
-
-io.on('connection', function(socket) {
-  socket.emit('change', {
-    DataUsu :DataUsu, 
-      DataLat: DataLat,
-      DataLong: DataLong,
-      DataFecha: DataFecha,
-      DataHora: DataHora 
+}, 3000);
+app.use(express.json({limit: '2mb'}));
+app.post('/historic', function (req, res) {
+  console.log("Historics sended")
+  console.log(req.body);
+  var HisDat = req.body;
+  var UserData=HisDat.datausua.toString();
+  var TSini=HisDat.datainicio.toString();
+  var TSfin=HisDat.datafin.toString();
+  console.log(UserData, TSini, TSfin)
+  con.query("SELECT * FROM gps WHERE Usuario=('"+UserData+"') AND TimeStamp BETWEEN ('"+TSini+"') AND ('"+TSfin+"');", function (err, rows) {
+    if (err) throw err;
+    HistData = JSON.parse(JSON.stringify(rows))
+    var DataHist = Object.values(HistData[0])
+    var DataTimeStamp= DataHist[4]
+    io.emit('timestamp', {
+      DataTimeStamp: DataTimeStamp,
+    });
+    io.on('connection', function (socket) {
+      socket.emit('timestamp', {
+        DataTimeStamp: DataTimeStamp
+      });
+    });
   });
-
-
-  
-
-
 });
-});
-},3000);
