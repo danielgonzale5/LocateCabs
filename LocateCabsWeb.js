bf53acbb6763d1b7e49a80fd674e030c37f02c6c
danielgonzale5 <dagv2000@gmail.com>
2021-09-29 20:06:45 -0500

    Trial

--- LocateCabsWeb.js
+++ LocateCabsWeb.js
@@ -166,7 +166,7 @@ app.post('/historic', function (req, res) {
     var CoordinatesArrTemp = []
     var CoordinatesArr = []
     for (var i = 0; i < DataHist.length; i++) {
-      CoordinatesArrTemp = [HistData[i][2],HistData[i][3]];
+      CoordinatesArrTemp = [DataHist[i][2],DataHist[i][3]];
       CoordinatesArr.push(CoordinatesArrTemp);
    }
     var DataTimeStamp= CoordinatesArr
