642a300eea65a3c5dc93efab63cfd9ceb185ed93
jricoj <jricoj@uninorte.edu.co>
2021-10-21 12:51:49 -0500

    Avance 2 Vehiculos

--- LocateCabsWeb.js
+++ LocateCabsWeb.js
@@ -8,6 +8,7 @@ var DatosGPS;
 var udp = require('dgram');
 var dir = __dirname;
 var io = require('socket.io')(server);
+io.setMaxListeners(0);
 //Metodos de conección Frontend-Backend, Rutas
 app.post('/github', function (req, res) {
   console.log("received")
@@ -32,12 +33,6 @@ app.get('/historicos', function (req, res) {
   res.sendfile(dir + '/historicos.html');
 });
 
-app.get('/historicostest', function (req, res) {
-  res.sendfile(dir + '/historicos_test.html');
-});
-app.get('/indextest', function (req, res) {
-  res.sendfile(dir + '/index_test.html');
-});
 
 //Conexión al puerto establecido
 server.listen(port, function (error) {
@@ -71,6 +66,29 @@ con.connect((err) => {
     console.log('la conexión con la base de datos funciona')
   }
 })
+
+let Usersvec;
+
+//Consulta a la base de datos por primera vez y manda el vector de usuarios para la lista desplegable----
+setTimeout(function(){
+
+  con.query('SELECT DISTINCT Usuario FROM gps ORDER BY Usuario', function(err, users){
+
+    if (err) throw err;
+    Usersvec = users.map(e=> e.Usuario);
+    console.log('El vector de usuarios inicial es:'); 
+    console.log(Usersvec);
+
+    io.on('connection', function (socket) {
+      socket.emit('init_users', {
+        Usersvec: Usersvec
+      });
+    });
+
+  });
+
+}, 0);
+
 //Ingreso de variables ppro defecto
 var Imysql = "INSERT INTO gps (Usuario, Latitud, Longitud, TimeStamp) VALUES ?";
 var values = [
@@ -105,7 +123,24 @@ serverudp.on('message', function (msg, info) {
   con.query(Imysql, [values], function (err, result) {
     if (err) throw err;
     console.log("Records inserted: " + result.affectedRows);
+    
   });
+
+  
+      
+    con.query('SELECT DISTINCT Usuario FROM gps ORDER BY Usuario', function(err, users){
+
+      if (err) throw err;
+      Usersvec = users.map(e=> e.Usuario);
+
+    });
+
+    if (!Usersvec.includes(DatosGPS[0])){
+    io.emit('new_user_detected', {
+      Newuser: DatosGPS[0]
+    });
+    }
+
 });
 //Mensaje de cierre del socket (Servidor UDP)
 serverudp.on('close', function () {
@@ -113,11 +148,61 @@ serverudp.on('close', function () {
 });
 //Puerto 3020
 serverudp.bind(3020);
-//Tiempo de espera para el cierre del socket
-setTimeout(function () {
-  serverudp.close();
-}, 999999999);
+
 //Consulta a la base de datos y conexión constante Backend-Frontend
+setInterval(function () {
+  con.query('SELECT * FROM gps WHERE Usuario = 1 ORDER BY idGPS DESC LIMIT 1', function (err, rows) {
+    if (err) throw err;
+    data = JSON.parse(JSON.stringify(rows))
+    var dataGPS = Object.values(data[0])
+    var DataUsu = dataGPS[1]
+    var DataLat = parseFloat(dataGPS[2]).toFixed(6)
+    var DataLong = parseFloat(dataGPS[3]).toFixed(6)
+    var DataTime = parseFloat(dataGPS[4])
+    io.emit('change1', {
+      DataUsu: DataUsu,
+      DataLat: DataLat,
+      DataLong: DataLong,
+      DataTime: DataTime,
+    });
+    io.on('connection', function (socket) {
+      socket.emit('change1', {
+        DataUsu: DataUsu,
+        DataLat: DataLat,
+        DataLong: DataLong,
+        DataTime: DataTime,
+      });
+    });
+  });
+}, 1500);
+
+setInterval(function () {
+    con.query('SELECT * FROM gps WHERE Usuario = 2 ORDER BY idGPS DESC LIMIT 1', function (err, rows) {
+      if (err) throw err;
+      data = JSON.parse(JSON.stringify(rows))
+      var dataGPS = Object.values(data[0])
+      var DataUsu = dataGPS[1]
+      var DataLat = parseFloat(dataGPS[2]).toFixed(6)
+      var DataLong = parseFloat(dataGPS[3]).toFixed(6)
+      var DataTime = parseFloat(dataGPS[4])
+      io.emit('change2', {
+        DataUsu: DataUsu,
+        DataLat: DataLat,
+        DataLong: DataLong,
+        DataTime: DataTime,
+      });
+      io.on('connection', function (socket) {
+        socket.emit('change2', {
+          DataUsu: DataUsu,
+          DataLat: DataLat,
+          DataLong: DataLong,
+          DataTime: DataTime,
+        });
+      });
+    });
+  }, 1500);
+
+//Consulta nuevamente la base de datos y conexión constante Backend-Frontend (Históricos)
 setInterval(function () {
   con.query('SELECT * FROM gps ORDER BY idGPS DESC LIMIT 1', function (err, rows) {
     if (err) throw err;
@@ -127,6 +212,7 @@ setInterval(function () {
     var DataLat = parseFloat(dataGPS[2]).toFixed(6)
     var DataLong = parseFloat(dataGPS[3]).toFixed(6)
     var DataTime = parseFloat(dataGPS[4])
+
     io.emit('change', {
       DataUsu: DataUsu,
       DataLat: DataLat,
@@ -142,7 +228,9 @@ setInterval(function () {
       });
     });
   });
-}, 50);
+
+  
+}, 1500);
 
 app.use(express.json({ limit: '500mb' }));
 
@@ -152,8 +240,9 @@ app.post('/historic', function (req, res) {
   var HisDat = req.body;
   var TSini = HisDat.datainicio.toString();
   var TSfin = HisDat.datafin.toString();
-  console.log(TSini, TSfin)
-  con.query("SELECT * FROM gps WHERE TimeStamp BETWEEN ('" + TSini + "') AND ('" + TSfin + "');", function (err, rows) {
+  var TSuser = HisDat.datauser.toString();
+  console.log(TSini, TSfin, TSuser)
+  con.query("SELECT * FROM gps WHERE TimeStamp BETWEEN ('" + TSini + "') AND ('" + TSfin + "') AND Usuario = '"+ TSuser +"';", function (err, rows) {
     if (err) throw err;
     var HistData = JSON.parse(JSON.stringify(rows))
     var DataHist = Object.values(HistData)
@@ -177,12 +266,7 @@ app.post('/historic', function (req, res) {
       DataUsuario: DataUsuario,
       DataTimeStamp: DataTimeStamp
     });
-    io.on('connection', function (socket) {
-      socket.emit('timestamp', {
-        DataUsuario: DataUsuario,
-        DataTimeStamp: DataTimeStamp
-      });
-    });
+
   });
   res.json({
     status: 'received'
